e58e08972a95ea01b42588e456e248b8486bd27f
shambhu-codes <shambhugouda520@gmail.com>
2025-06-25 18:27:00 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,27 @@
+class Student {
+    private int id;
+    private String name;
+    private double marks;
+
+    public Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Marks: " + marks;
+    }
+}
